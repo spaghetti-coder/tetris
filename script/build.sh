49b9b6b2de244e9dad5f2f638caddcd0dc7528b0
Spaghetti <spaghetti.coder.ru@gmail.com>
2026-07-29 12:46:37 +0300

    WiP

--- script/build.sh
+++ script/build.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+OUT_FILE="${1-index.html}"
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
 SRC_DIR="${ROOT_DIR}/src"
-OUT_FILE="${ROOT_DIR}/index.html"
 
 CSS_MARKER=__app.css__
 JS_MARKER=__app.js__
 
+cd "${ROOT_DIR}"
+
 sed -e '/\/\*\s*'"${CSS_MARKER//./\.}"'\s*\*\//{' \
     -e "r ${SRC_DIR}/app.css" \
     -e 'd' \
-    -e '}' "${SRC_DIR}/index.html" \
+    -e '}' "${SRC_DIR}/template.html" \
 | sed -e '/\/\*\s*'"${JS_MARKER//./\.}"'\s*\*\//{' \
       -e "r ${SRC_DIR}/app.js" \
       -e 'd' \
